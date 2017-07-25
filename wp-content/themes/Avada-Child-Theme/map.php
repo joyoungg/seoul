65979af06ceb3d48613e6cdbaec494d172e7bc94
김재국 <luritas@duse.co.kr>
2017-07-25 17:14:49 +0900

    뼈대 완성

---
+++ wp-content/themes/Avada-Child-Theme/map.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Template name: map view
+ */
+
+get_header();
+
+?>
+    <style>
+        #main {
+            padding: 0;
+        }
+        #main div.fusion-row {
+            max-width: 100%;
+        }
+    </style>
+
+    <img src="<?=get_template_directory_uri()?>-Child-Theme/images/map.png" alt="">
+
+<?php
+get_footer();
+
+
